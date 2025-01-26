f91f6eb19fb61c2c704c0a0920a5cfb0dc553a08
Akash Adhya <akashadhya19@gmail.com>
2025-01-26 12:52:15 +0530

    single quotation part

--- theMainFile.cpp
+++ theMainFile.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <sstream>
 #include <algorithm>
 #include <cctype>
 #include <locale>
@@ -13,20 +14,28 @@
 using namespace std;
 
 // Helper functions to trim strings
-inline void ltrim(string &s) {
-    s.erase(s.begin(), find_if(s.begin(), s.end(), [](unsigned char ch) { return !isspace(ch); }));
+inline void ltrim(string &s)
+{
+    s.erase(s.begin(), find_if(s.begin(), s.end(), [](unsigned char ch)
+                               { return !isspace(ch); }));
 }
 
-inline void rtrim(string &s) {
-    s.erase(find_if(s.rbegin(), s.rend(), [](unsigned char ch) { return !isspace(ch); }).base(), s.end());
+inline void rtrim(string &s)
+{
+    s.erase(find_if(s.rbegin(), s.rend(), [](unsigned char ch)
+                    { return !isspace(ch); })
+                .base(),
+            s.end());
 }
 
 // Partition the command from the input
-string partitionCommand(const string &input) {
+string partitionCommand(const string &input)
+{
     string command = "";
     int i = 0;
 
-    while (i < input.length() && input[i] != ' ') {
+    while (i < input.length() && input[i] != ' ')
+    {
         command += input[i];
         i++;
     }
@@ -35,17 +44,20 @@ string partitionCommand(const string &input) {
 }
 
 // Partition parameters from the input
-string partitionParameters(string input, int index) {
+string partitionParameters(string input, int index)
+{
     return input.substr(index);
 }
 
 // Function to check if a command is a built-in
-bool isBuiltin(const string &command, const vector<string> &builtins) {
+bool isBuiltin(const string &command, const vector<string> &builtins)
+{
     return find(builtins.begin(), builtins.end(), command) != builtins.end();
 }
 
 // Search for an executable in the PATH
-string findExecutable(const string &command) {
+string findExecutable(const string &command)
+{
     char *path_env = getenv("PATH");
     if (path_env == nullptr)
         return "";
@@ -53,20 +65,24 @@ string findExecutable(const string &command) {
     string path(path_env);
     size_t pos = 0;
 
-    while ((pos = path.find(';')) != string::npos) { // Use ';' for Windows PATH delimiter
+    while ((pos = path.find(';')) != string::npos)
+    { // Use ';' for Windows PATH delimiter
         string dir = path.substr(0, pos);
         path.erase(0, pos + 1);
 
         string file_path = dir + "\\" + command;
-        if (_access(file_path.c_str(), 4) == 0) { // Check for read and execute permissions
+        if (_access(file_path.c_str(), 4) == 0)
+        {                     // Check for read and execute permissions
             return file_path; // Return the first match found
         }
     }
 
     // Check the remaining part of PATH
-    if (!path.empty()) {
+    if (!path.empty())
+    {
         string file_path = path + "\\" + command;
-        if (_access(file_path.c_str(), 4) == 0) {
+        if (_access(file_path.c_str(), 4) == 0)
+        {
             return file_path; // Return the first match found
         }
     }
@@ -75,9 +91,11 @@ string findExecutable(const string &command) {
 }
 
 // Execute external commands
-void executeExternal(const vector<string> &args) {
+void executeExternal(const vector<string> &args)
+{
     string commandLine;
-    for (const string &arg : args) {
+    for (const string &arg : args)
+    {
         commandLine += arg + " ";
     }
 
@@ -86,7 +104,8 @@ void executeExternal(const vector<string> &args) {
     PROCESS_INFORMATION pi;
 
     // Create the process
-    if (!CreateProcess(nullptr, const_cast<char *>(commandLine.c_str()), nullptr, nullptr, FALSE, 0, nullptr, nullptr, &si, &pi)) {
+    if (!CreateProcess(nullptr, const_cast<char *>(commandLine.c_str()), nullptr, nullptr, FALSE, 0, nullptr, nullptr, &si, &pi))
+    {
         cerr << "Error: failed to execute command\n";
         return;
     }
@@ -100,16 +119,79 @@ void executeExternal(const vector<string> &args) {
 }
 
 // Finding Present working directory
-void pwd() {
+void pwd()
+{
     char pwd[PATH_MAX];
-    if (_getcwd(pwd, sizeof(pwd)) != NULL) {
+    if (_getcwd(pwd, sizeof(pwd)) != NULL)
+    {
         cout << pwd << endl;
-    } else {
+    }
+    else
+    {
         perror("_getcwd() error");
     }
 }
 
-int main() {
+// Processing the Quotations
+string processQuotedSegments(const string& parameters) {
+    string result="";
+    bool isContainQuote = false;
+    int  n = parameters.length();
+
+    for(char c : parameters){
+        if(c == '\'' || c == '"'){
+            isContainQuote = true; 
+            break;
+        }
+    }
+
+    if(!isContainQuote){
+        stringstream ss(parameters);
+        string word;
+        while (ss >> word)
+        {
+            result += word+" ";
+        }
+        
+    }
+    else{
+        bool isQuoted = false;
+        string temp = "";
+        int i = 0;
+        while(i < n){
+            char c = parameters[i];
+
+             if (c == '\'' || c == '"') {
+                if (isQuoted) {
+                    result += temp;
+                    temp = "";
+                    isQuoted = false;
+                } else {
+                    isQuoted = true;
+                    temp = ""; 
+                }
+            } else if (isQuoted) {
+                temp += c;
+            } else {
+                result += c;
+            }
+
+            i++;
+        }
+    }
+
+    return result;
+}
+
+
+// Echo command
+void echo(const string& parameters) {
+    string processed = processQuotedSegments(parameters);
+    cout << processed << endl; // Print the processed result
+}
+
+int main()
+{
     // List of built-in commands
     vector<string> builtins = {"type", "echo", "exit", "pwd"};
 
@@ -117,32 +199,32 @@ int main() {
     cout << unitbuf;
     cerr << unitbuf;
 
-    cout<<"Hello there! Welcome to my shell. Use it however you want."<<endl;
-    cout<<"Created with LOVE by Akash ❤"<<endl;
+    cout << "Hello there! Welcome to my shell. Use it however you want." << endl;
+    cout << "Created with LOVE by Akash ❤" << endl;
 
-    while (true) {
+    while (true)
+    {
         cout << "$ ";
 
         string input;
         getline(cin, input);
 
-        
         // Capture command and parameters
         string command = partitionCommand(input);
         string parameters = partitionParameters(input, command.length());
 
-        
         ltrim(command);
         rtrim(command);
         ltrim(parameters);
         rtrim(parameters);
 
         // Exiting the shell
-        if (command == "exit") {
-            if(parameters == "0")
+        if (command == "exit")
+        {
+            if (parameters == "0")
                 exit(0);
-            else   
-                cout<<"Use 'exit 0' to exit."<<endl;
+            else
+                cout << "Use 'exit 0' to exit." << endl;
         }
 
         // Split input into command and arguments
@@ -150,80 +232,101 @@ int main() {
         args.push_back(command);
         size_t pos = 0;
         string temp = parameters;
-        while ((pos = temp.find(' ')) != string::npos) {
+        while ((pos = temp.find(' ')) != string::npos)
+        {
             args.push_back(temp.substr(0, pos));
             temp.erase(0, pos + 1);
         }
-        if (!temp.empty()) {
+        if (!temp.empty())
+        {
             args.push_back(temp);
         }
 
         // Handle the `echo` command
-        if (command == "echo") {
-            int i = 0;
-            while (i < parameters.length()) {
-                if (parameters[i] == '\'' || parameters[i] == '\"') {
-                    parameters.erase(parameters.begin() + i);
-                    continue;
-                }
-                i++; 
-            }
-            cout << parameters << endl;
+        if (command == "echo")
+        {
+            echo(parameters);
+        }
+
+        // Handle the cat Command
+        else if(command == "cat"){
+            system(input.c_str());
         }
 
         // Handle the `pwd` command
-        else if (command == "pwd") {
+        else if (command == "pwd")
+        {
             if (parameters.empty())
                 pwd();
-            else {
+            else
+            {
                 cerr << "pwd : No parameters required." << endl;
             }
         }
 
         // Handle the `cd` command
-        else if(command == "cd"){
+        else if (command == "cd")
+        {
             const char *targetDir = parameters.c_str();
-            if(parameters == "~"){
+            if (parameters == "~")
+            {
                 const char *HOMEPATH = getenv("USERPROFILE");
-                if(HOMEPATH && _chdir(HOMEPATH) == 0){
+                if (HOMEPATH && _chdir(HOMEPATH) == 0)
+                {
                     // Successfully changed to home directory.
                 }
-                else{
+                else
+                {
                     cout << command << ": " << parameters << ": Unable to access home directory" << endl;
                 }
             }
-            else if(_chdir(targetDir) != 0){
-                cout<<command<<": "<<parameters<<" No such file or directory";
+            else if (_chdir(targetDir) != 0)
+            {
+                cout << command << ": " << parameters << " No such file or directory";
             }
         }
 
         // Handle the `type` command
-        else if (command == "type") {
-            if (parameters.empty()) {
+        else if (command == "type")
+        {
+            if (parameters.empty())
+            {
                 cerr << "type: missing argument\n";
                 continue;
             }
 
-            if (isBuiltin(parameters, builtins)) {
+            if (isBuiltin(parameters, builtins))
+            {
                 cout << parameters << " is a shell builtin\n";
-            } else if (parameters.find('\\') != string::npos && _access(parameters.c_str(), 4) == 0) {
+            }
+            else if (parameters.find('\\') != string::npos && _access(parameters.c_str(), 4) == 0)
+            {
                 cout << parameters << " is " << parameters << "\n";
-            } else {
+            }
+            else
+            {
                 string execPath = findExecutable(parameters);
-                if (!execPath.empty()) {
+                if (!execPath.empty())
+                {
                     cout << parameters << " is " << execPath << "\n";
-                } else {
+                }
+                else
+                {
                     cerr << parameters << ": not found\n";
                 }
             }
         }
 
         // Handle unknown or external commands
-        else {
+        else
+        {
             string execPath = findExecutable(command);
-            if (!execPath.empty()) {
+            if (!execPath.empty())
+            {
                 executeExternal(args);
-            } else {
+            }
+            else
+            {
                 cerr << command << ": command not found\n";
             }
         }
